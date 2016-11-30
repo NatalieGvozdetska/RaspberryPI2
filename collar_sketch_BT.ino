fb6c8943a74bca164853c729be3b9c30b3bdd0c4
NatalieGvozdetska <n.gvozdetska@gmail.com>
2016-11-30 13:39:53 +0300

    Add files via upload

---
+++ collar_sketch_BT.ino
@@ -0,0 +1,412 @@
+#include <SoftwareSerial.h>  
+#include "bluetooth.h"
+#include "OneWire.h"
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+#include <ArduinoJson.h>
+
+#define RxD 6
+#define TxD 5
+OneWire  ds(10);  // on pin 10 (a 4.7K resistor is necessary)
+SoftwareSerial BlueToothSerial(RxD,TxD);
+SoftwareSerial mySerial(11, 12); // RX, TX
+TinyGPS gps;
+void gpsdump(TinyGPS &gps);
+void printFloat(double f, int digits = 2);
+
+int val;
+float cel;
+
+//  VARIABLES
+int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+int blinkPin = 8;                // pin to blink led at each beat
+int fadePin = 7;                  // pin to do fancy classy fading blink at each beat
+int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
+// these variables are volatile because they are used during the interrupt service routine!
+volatile int BPM;                   // used to hold the pulse rate
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // holds the time between beats, the Inter-Beat Interval
+volatile boolean Pulse = false;     // true when pulse wave is high, false when it's low
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+volatile int rate[10];                    // used to hold last ten IBI values
+volatile unsigned long sampleCounter = 0;          // used to determine pulse timing
+volatile unsigned long lastBeatTime = 0;           // used to find the inter beat interval
+volatile int P =512;                      // used to find peak in pulse wave
+volatile int T = 512;                     // used to find trough in pulse wave
+volatile int thresh = 512;                // used to find instant moment of heart beat
+volatile int amp = 100;                   // used to hold amplitude of pulse waveform
+volatile boolean firstBeat = true;        // used to seed rate array so we startup with reasonable BPM
+volatile boolean secondBeat = true;       // used to seed rate array so we startup with reasonable BPM
+void setup()
+{
+   Serial.begin(38400);     
+   BlueToothSerial.begin(38400); 
+     mySerial.begin(9600);
+     delay(1000);
+  pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
+  pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
+             // sets up to read Pulse Sensor signal every 2mS 
+ 
+}
+void loop() {
+  bool newdata = false;
+  unsigned long start = millis();
+  uninterruptSetup();    
+  
+  delay(20);                             //  take a break
+
+  Serial.println();
+
+
+  // Every 5 seconds we print an update
+  while (millis() - start < 5000) {
+    if (mySerial.available()) {
+      char c = mySerial.read();
+      // Serial.print(c);  // uncomment to see raw GPS data
+      if (gps.encode(c)) {
+        newdata = true;
+        // break;  // uncomment to print new data immediately!
+      }
+    }
+  }
+  if (newdata) {
+      interruptSetup();    
+      sendDataToProcessing('S', Signal);     // send Processing the raw Pulse Sensor data
+  if (QS == true){                       // Quantified Self flag is true when arduino finds a heartbeat
+        fadeRate = 255;                  // Set 'fadeRate' Variable to 255 to fade LED with pulse
+        sendDataToProcessing('B',BPM);   // send heart rate with a 'B' prefix
+        sendDataToProcessing('Q',IBI);   // send time between beats with a 'Q' prefix
+        QS = false;                      // reset the Quantified Self flag for next time    
+     }
+  
+  ledFadeToBeat();
+    gpsdump(gps);
+  }
+ else {
+    interruptSetup();    
+    sendDataToProcessing('S', Signal);     // send Processing the raw Pulse Sensor data
+  if (QS == true){                       // Quantified Self flag is true when arduino finds a heartbeat
+        fadeRate = 255;                  // Set 'fadeRate' Variable to 255 to fade LED with pulse
+        sendDataToProcessing('B',BPM);   // send heart rate with a 'B' prefix
+        sendDataToProcessing('Q',IBI);   // send time between beats with a 'Q' prefix
+        QS = false;                      // reset the Quantified Self flag for next time    
+     }
+  
+  ledFadeToBeat();
+   gpsnone();
+   }
+
+
+  {
+
+delay(1000);
+}
+
+    // BlueToothSerial.print(Temperature());
+     delay(2000);
+}
+ 
+float Temperature() {
+ byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+   
+  if ( !ds.search(addr)) {
+
+    ds.reset_search();
+    delay(250);
+  }
+   
+
+  for( i = 0; i < 8; i++) {
+
+  }
+ 
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+
+  }
+
+  
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+     
+      type_s = 1;
+      break;
+    case 0x28:
+
+      type_s = 0;
+      break;
+    case 0x22:
+
+      type_s = 0;
+      break;
+    default:
+    break;
+     
+  } 
+ 
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+   
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+   
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+
+  }
+
+ 
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+return celsius;
+}
+
+void gpsdump(TinyGPS &gps)
+{
+  long lat, lon;
+  float flat, flon;
+  unsigned long age, date, time, chars;
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+  unsigned short sentences, failed;
+  int i;
+  float val;
+  char buff[10];
+  char valueString[100] = "";
+
+
+  gps.f_get_position(&flat, &flon, &age);
+    val = analogRead(10);
+
+float cel = Temperature();
+
+StaticJsonBuffer<200> jsonBuffer;
+ JsonObject& root = jsonBuffer.createObject();
+ //JsonArray& data = root.createNestedArray("gps");
+ //data.add("Latitude");
+  root["Lat"]=(double_with_n_digits(flat, 6));
+//  data.add("Longtitude");
+  root["Lon"]=(double_with_n_digits(flon, 6));
+  root["Temp"] = cel;
+
+// root.printTo(Serial);
+ //root.printTo(BlueToothSerial);
+float lati=double_with_n_digits(flat, 6);
+ float longti=double_with_n_digits(flon, 6);
+ dtostrf(lati, 6, 6, buff);
+     strcat(valueString, buff);
+  strcat(valueString, ",");
+ dtostrf(longti, 6,6, buff);
+     strcat(valueString, buff);
+  strcat(valueString, ",");
+   dtostrf(cel, 6, 2, buff);
+     strcat(valueString, buff);
+       strcat(valueString, ", ");
+       dtostrf(BPM, 6, 2, buff);
+        strcat(valueString, buff);
+  strcat(valueString, ";");
+ //BlueToothSerial.println(lati);
+// BlueToothSerial.println(longti);
+// BlueToothSerial.println(cel);
+//Serial.println(buff);
+Serial.println(valueString);
+BlueToothSerial.println(valueString);
+
+}
+
+void printFloat(double number, int digits)
+{
+  // Handle negative numbers
+  if (number < 0.0) {
+     Serial.print('-');
+     number = -number;
+  }
+
+  // Round correctly so that print(1.999, 2) prints as "2.00"
+  double rounding = 0.5;
+  for (uint8_t i=0; i<digits; ++i)
+    rounding /= 10.0;
+  
+  number += rounding;
+
+  // Extract the integer part of the number and print it
+  unsigned long int_part = (unsigned long)number;
+  double remainder = number - (double)int_part;
+  Serial.print(int_part);
+
+  // Print the decimal point, but only if there are digits beyond
+  if (digits > 0)
+    Serial.print("."); 
+
+  // Extract digits from the remainder one at a time
+  while (digits-- > 0) {
+    remainder *= 10.0;
+    int toPrint = int(remainder);
+    Serial.print(toPrint);
+    remainder -= toPrint;
+  }
+}
+
+void gpsnone(){
+    int i;
+  float val;
+  char buff[10];
+  char valueString[100] = "";
+  float cel=Temperature();
+  float lati=0;
+ float longti=0;
+ dtostrf(lati, 6, 6, buff);
+     strcat(valueString, buff);
+  strcat(valueString, ", ");
+ dtostrf(longti, 6, 6, buff);
+     strcat(valueString, buff);
+  strcat(valueString, ", ");
+   dtostrf(cel, 6, 2, buff);
+     strcat(valueString, buff);
+       strcat(valueString, ", ");
+       dtostrf(BPM, 6, 2, buff);
+        strcat(valueString, buff);
+  strcat(valueString, ";");
+ //BlueToothSerial.println(lati);
+// BlueToothSerial.println(longti);
+// BlueToothSerial.println(cel);
+//Serial.println(buff);
+Serial.println(valueString);
+BlueToothSerial.println(valueString);
+}
+
+
+void ledFadeToBeat(){
+    fadeRate -= 15;                         //  set LED fade value
+    fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
+    analogWrite(fadePin,fadeRate);          //  fade LED
+  }
+
+
+void sendDataToProcessing(char symbol, int data ){
+  }
+void interruptSetup(){     
+  // Initializes Timer2 to throw an interrupt every 2mS.
+  TCCR2A = 0x02;     // DISABLE PWM ON DIGITAL PINS 3 AND 11, AND GO INTO CTC MODE
+  TCCR2B = 0x06;     // DON'T FORCE COMPARE, 256 PRESCALER 
+  OCR2A = 0X7C;      // SET THE TOP OF THE COUNT TO 124 FOR 500Hz SAMPLE RATE
+  TIMSK2 = 0x02;     // ENABLE INTERRUPT ON MATCH BETWEEN TIMER2 AND OCR2A
+  sei();             // MAKE SURE GLOBAL INTERRUPTS ARE ENABLED      
+} 
+void uninterruptSetup(){     
+  // Initializes Timer2 to throw an interrupt every 2mS.
+  TCCR2A = 0;     // DISABLE PWM ON DIGITAL PINS 3 AND 11, AND GO INTO CTC MODE
+
+  TIMSK2 = 0;     // ENABLE INTERRUPT ON MATCH BETWEEN TIMER2 AND OCR2A
+  
+} 
+
+// THIS IS THE TIMER 2 INTERRUPT SERVICE ROUTINE. 
+// Timer 2 makes sure that we take a reading every 2 miliseconds
+ISR(TIMER2_COMPA_vect){                         // triggered when Timer2 counts to 124
+    cli();                                      // disable interrupts while we do this
+    Signal = analogRead(pulsePin);              // read the Pulse Sensor 
+    sampleCounter += 2;                         // keep track of the time in mS with this variable
+    int N = sampleCounter - lastBeatTime;       // monitor the time since the last beat to avoid noise
+
+//  find the peak and trough of the pulse wave
+    if(Signal < thresh && N > (IBI/5)*3){       // avoid dichrotic noise by waiting 3/5 of last IBI
+        if (Signal < T){                        // T is the trough
+            T = Signal;                         // keep track of lowest point in pulse wave 
+         }
+       }
+      
+    if(Signal > thresh && Signal > P){          // thresh condition helps avoid noise
+        P = Signal;                             // P is the peak
+       }                                        // keep track of highest point in pulse wave
+    
+  //  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
+  // signal surges up in value every time there is a pulse
+if (N > 250){                                   // avoid high frequency noise
+  if ( (Signal > thresh) && (Pulse == false) && (N > (IBI/5)*3) ){        
+    Pulse = true;                               // set the Pulse flag when we think there is a pulse
+    digitalWrite(blinkPin,HIGH);                // turn on pin 13 LED
+    IBI = sampleCounter - lastBeatTime;         // measure time between beats in mS
+    lastBeatTime = sampleCounter;               // keep track of time for next pulse
+         
+         if(firstBeat){                         // if it's the first time we found a beat, if firstBeat == TRUE
+             firstBeat = false;                 // clear firstBeat flag
+             return;                            // IBI value is unreliable so discard it
+            }   
+         if(secondBeat){                        // if this is the second beat, if secondBeat == TRUE
+            secondBeat = false;                 // clear secondBeat flag
+               for(int i=0; i<=9; i++){         // seed the running total to get a realisitic BPM at startup
+                    rate[i] = IBI;                      
+                    }
+            }
+          
+    // keep a running total of the last 10 IBI values
+    word runningTotal = 0;                   // clear the runningTotal variable    
+
+    for(int i=0; i<=8; i++){                // shift data in the rate array
+          rate[i] = rate[i+1];              // and drop the oldest IBI value 
+          runningTotal += rate[i];          // add up the 9 oldest IBI values
+        }
+        
+    rate[9] = IBI;                          // add the latest IBI to the rate array
+    runningTotal += rate[9];                // add the latest IBI to runningTotal
+    runningTotal /= 15;                     // average the last 10 IBI values 
+    BPM = 60000/runningTotal;               // how many beats can fit into a minute? that's BPM!
+    QS = true;                              // set Quantified Self flag 
+    // QS FLAG IS NOT CLEARED INSIDE THIS ISR
+    }                       
+}
+
+  if (Signal < thresh && Pulse == true){     // when the values are going down, the beat is over
+      digitalWrite(blinkPin,LOW);            // turn off pin 13 LED
+      Pulse = false;                         // reset the Pulse flag so we can do it again
+      amp = P - T;                           // get amplitude of the pulse wave
+      thresh = amp/2 + T;                    // set thresh at 50% of the amplitude
+      P = thresh;                            // reset these for next time
+      T = thresh;
+     }
+  
+  if (N > 2500){                             // if 2.5 seconds go by without a beat
+      thresh = 512;                          // set thresh default
+      P = 512;                               // set P default
+      T = 512;                               // set T default
+      lastBeatTime = sampleCounter;          // bring the lastBeatTime up to date        
+      firstBeat = true;                      // set these to avoid noise
+      secondBeat = true;                     // when we get the heartbeat back
+     }
+  
+  sei();                                     // enable interrupts when youre done!
+}// end isr
+
+
+
+
+
